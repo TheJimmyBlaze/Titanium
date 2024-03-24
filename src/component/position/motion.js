b9c55afd5e988ee0d21d84d99c56a561f58c7b61
Cameron Taylor <TheJimmyBlaze@gmail.com>
2024-03-24 20:33:01 +1300

    Change how motion accelerates diagonally

--- src/component/position/motion.js
+++ src/component/position/motion.js
@@ -8,6 +8,8 @@ export const useMotion = ({
 }) => {
 
     const state = {
+        potentialX: 0,
+        potentialY: 0,
         velocityX,
         velocityY,
         acceleration,
@@ -25,17 +27,33 @@ export const useMotion = ({
         velocityY: state.velocityY
     });
 
-    const accelerateX = () => state.velocityX += state.acceleration;
-    const decelerateX = () => state.velocityX -= state.acceleration;
-    const accelerateY = () => state.velocityY += state.acceleration;
-    const decelerateY = () => state.velocityY -= state.acceleration;
+    const accelerateX = () => state.potentialX += state.acceleration;
+    const decelerateX = () => state.potentialX -= state.acceleration;
+    const accelerateY = () => state.potentialY += state.acceleration;
+    const decelerateY = () => state.potentialY -= state.acceleration;
 
     const apply = position => {
+
+        //Share potential velocity
+        if (
+            Math.abs(state.velocityX) > 0.01 && 
+            Math.abs(state.velocityY) > 0.01
+        ) {
+            state.potentialX *= 0.75;
+            state.potentialY *= 0.75;
+        }
         
+        state.velocityX += state.potentialX;
+        state.velocityY += state.potentialY;
+        state.potentialX = 0;
+        state.potentialY = 0;
+        
+        //Apply drag
         const invertedDrag = 1 - state.drag;
         state.velocityX *= invertedDrag;
         state.velocityY *= invertedDrag;
 
+        //Apply velocity
         const deltaX = state.velocityX * deltaTime();
         const deltaY = state.velocityY * deltaTime();
         position.move(deltaX, deltaY);
